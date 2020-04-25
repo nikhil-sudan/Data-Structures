2ac7af234cdfc5801147cb264ac4614107a98bd5
nikhil-sudan <nikhil.sudan1201@gmail.com>
2020-04-25 18:11:56 +0530

    height of tree file added

---
+++ Binary trees/height of a tree.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 16:54:51 2020
+
+@author: Nikhil Sudan
+"""
+
+# IDEA:
+#     pass
+# INTRODUCTION:
+#     pass
+class Node:
+    def __init__(self, value):
+        self.value=value
+        self.left=None
+        self.right=None
+        self.height=None
+        
+class BinaryTree:
+    def __init__(self, root):
+        self.root=Node(root)
+    
+    def height(self, start):
+        if (start==None):
+            return -1
+        self.left_height=self.height(start.left)
+        self.right_height= self.height(start.right)
+        
+        return (1+max(self.left_height, self.right_height))
+
+tree=BinaryTree(1)                          #        1
+tree.root.left=Node(2)                      #      /   \
+tree.root.right=Node(3)                     #     2     3
+tree.root.left.left=Node(4)                 #    / \   / \
+tree.root.left.right=Node(5)                #   4   5 6   7
+tree.root.right.left=Node(6)                    
+tree.root.right.right=Node(7)
+
+print("height of the tree is: ",tree.height(tree.root))
