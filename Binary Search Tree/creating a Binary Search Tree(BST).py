4901464e44f45c593c84f9f77fef890958666d1f
nikhil-sudan <nikhil.sudan1201@gmail.com>
2020-05-01 22:56:42 +0530

    added comment and in-order traversal for print

--- Binary Search Tree/creating a Binary Search Tree(BST).py
+++ Binary Search Tree/creating a Binary Search Tree(BST).py
@@ -8,15 +8,14 @@ Created on Fri May  1 18:50:33 2020
 IDEA:
     Binay search tree is a tree having following properties:
     The left subtree of a node contains only nodes with keys lesser than the node’s key.
-The right subtree of a node contains only nodes with keys greater than the node’s key.
-The left and right subtree each must also be a binary search tree.
+    The right subtree of a node contains only nodes with keys greater than the node’s key.
+    The left and right subtree each must also be a binary search tree.
 
 INTRODUCTION:
-    in this algorithm,we check if the root exist, otherwise we create a node and make it root node for BST
+    in this algorithm, we first check if the root exist, otherwise we create a node and make it root node for BST
     for each first value to be inserted:
           if that value is smaller than the parent node then we insert to LHS
           otherwise we make it the right node of the parent
-    we repeat the steps for each node to be inserted
 
 """
 
@@ -28,7 +27,7 @@ class Node:
         self.right = None 
 
 
-#return list of nodes in pre-order traversal
+#return list of nodes in PRE-ORDER traversal (root,left,right)
 def preOrder(root):
     if root == None:
         return
@@ -36,30 +35,37 @@ def preOrder(root):
     preOrder(root.left)
     preOrder(root.right)
     
+#return list of nodes in IN-ORDER  traversal (left,root,right)
+def inOrder(root):
+    if root == None:
+        return
+    inOrder(root.left)
+    print (root.info, end=" ")
+    inOrder(root.right)
+    
 class BinarySearchTree:
     def __init__(self): 
         self.root = None
 
-# method to perform insertion to form a binary search tree
+# insert() method to perform insertion to form a binary search tree
     def insert(self, val):
         Node1=self.root
         
-        #checking if root exist. If does not exist, then we create root node
+#checking if root exist. If does not exist, then we create root node
         if(Node1==None):  
             self.root=Node(val)
-    """            
-    if root already exist, then we traverse the tree till such that:
-    if value to be inserted is smaller than parent , then insert on LHS of parent:
-    else insert new value on RHS of parent node
-    """
+
+#finding the right position for the node and inserting it
         else:
             while(Node1):
+                #if value of the node is smaller than the parent than we move to LHS sub tree
                 if(val<Node1.info):
                     if(Node1.left):
                         Node1=Node1.left
                     else:
                         Node1.left=Node(val)
                         break
+                #if value of the node is greater than the parent than we move to RHS sub tree
                 else:
                     if(Node1.right):
                         Node1=Node1.right
@@ -69,11 +75,21 @@ class BinarySearchTree:
         return tree.root
 
 tree = BinarySearchTree()
-t = int(input())
+
+t = int(input())    #t denotes number of values to be inserted into the BST.
 
 arr = list(map(int, input().split()))
 
 for i in range(t):
     tree.insert(arr[i])
 
-preOrder(tree.root)
+#print("pre-order traversal of a BST is: ", end="")
+#preOrder(tree.root)  #prints pre-order traversal traveral of the BST
+
+
+print("in-order traversal of a BST is: ", end="")
+inOrder(tree.root)
+
+"""
+    ***NOTE: in-order traversal of a BST gives values in increasing order
+"""
